4a188fa7c18666504453e9b340186b588ebd6d79
tjsdla520 <83327352+tjsdla520@users.noreply.github.com>
2021-05-10 12:53:48 +0900

    최성철
    memberlogincontroller

--- Cinebro/src/cinebro/members/controller/MemberLogInController.java
+++ Cinebro/src/cinebro/members/controller/MemberLogInController.java
@@ -16,40 +16,34 @@ import cinebro.members.model.MemberDao;
 
 
 public class MemberLogInController<cinebro> extends SuperClass {
-	private String id ;
+	private String email ;
 	private String password  ;
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doGet(request, response);
 	
-		String gotopage = "/member/meLoginForm.jsp" ;
+		String gotopage = "/members/meLoginForm.jsp" ;
 		super.GotoPage(gotopage);
 	}	
 	@Override
 	public boolean validate(HttpServletRequest request) {
-		boolean isCheck = true ; // true이면 유효성 검사에 문제가 없습니다.
-		
-		if(this.id.length() < 4 || this.id.length() > 10) {
-			request.setAttribute(super.PREFIX + "id", "아이디는 4자리 이상 10자리 이하이어야 합니다."); 
-			isCheck = false ;
-		}
+		boolean isCheck = true ; 
 		
 		if(this.password.length() < 4 || this.password.length() > 10) {
 			request.setAttribute(super.PREFIX + "password", "비밀 번호는 4자리 이상 10자리 이하이어야 합니다."); 
 			isCheck = false ;
 		}
-				
 		return isCheck ;
 	}
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doPost(request, response);
 		
-		this.id = request.getParameter("id") ;
+		this.email = request.getParameter("email") ;
 		this.password = request.getParameter("password") ;
 		
-		System.out.println(id + "/" + password);
+		System.out.println(email + "/" + password);
 		
 		String gotopage = "" ;
 		
@@ -57,35 +51,29 @@ public class MemberLogInController<cinebro> extends SuperClass {
 			System.out.println("유효성 검사에 문제 없슴");
 			
 			MemberDao dao = new MemberDao();
-			Member bean = dao.SelectData(id, password) ;
+			Member bean = dao.SelectData(email, password) ;
 			
 			if (bean == null) { 
 				System.out.println("로그인 실패");
-				String message = "아이디나 비밀 번호가 잘못되었습니다." ;
+				String message = "이메일 혹은 비밀 번호가 잘못되었습니다." ;
 				super.setErrorMessage(message);
-				gotopage = "/member/meLoginForm.jsp" ;
+				gotopage = "/members/meLoginForm.jsp" ;
 				super.GotoPage(gotopage); 				
 			} else { 
 				System.out.println("로그인 성공");
-				// 로그인 정보를 세션에 바인딩
 				super.session.setAttribute("loginfo", bean);
 				
 				
-				MemberDao mdao = new MemberDao() ;
-				
-				// 쇼핑 정보 읽어 옵니다.
-				List<cinebro> lists = mdao.Getcinebro(bean.getId()) ;
-				
-				if (lists.size() > 0) {
-					ItemList mycart = new ItemList(); 
-				
-					// insert mycart for statement
-					for(cinebro shop : lists) {
-						 
-					}
-					
-					super.session.setAttribute("mycart", mycart);
-				}
+//				MemberDao mdao = new MemberDao() ;
+//				// 쇼핑에 장바구니가 있듯이 재생중인 영화를 바인딩
+//				List<cinebro> lists = mdao.Getcinebro(bean.getId()) ;
+//				if (lists.size() > 0) {
+//					ItemList mycart = new ItemList(); 
+//					// insert mycart for statement
+//					for(cinebro shop : lists) {
+//					}
+//					super.session.setAttribute("mycart", mycart);
+//				}
 				
 				// 메인 페이지로 이동합니다.
 				new MainController().doGet(request, response); 
@@ -94,10 +82,10 @@ public class MemberLogInController<cinebro> extends SuperClass {
 			
 		} else { // 문제가 있슴
 			// 이전에 입력했던 정보를 다시 바인딩해줍니다.
-			request.setAttribute("id", this.id);
+			request.setAttribute("email", this.email);
 			request.setAttribute("password", this.password);
 			
-			gotopage = "/member/meLoginForm.jsp" ;
+			gotopage = "/members/meLoginForm.jsp" ;
 			super.GotoPage(gotopage); 
 		}
 	}
