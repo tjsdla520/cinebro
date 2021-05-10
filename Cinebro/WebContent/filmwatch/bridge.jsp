ab8a1c59c5cf7f7283b775ced5934fe03686c4c9
tjsdla520 <83327352+tjsdla520@users.noreply.github.com>
2021-05-10 17:44:33 +0900

    아싸뵤 내껀 된다리

--- Cinebro/WebContent/filmwatch/bridge.jsp
+++ Cinebro/WebContent/filmwatch/bridge.jsp
@@ -8,9 +8,38 @@
 <body>
 	<%-- <%@ include file="header.jsp" %> --%>
 	<%-- <jsp:include page="<%=contextPath%>/anime-main/header.jsp"/> --%>
-	<jsp:include page="./../anime-main/header.jsp"/>
-	bridge.jsp
-	
-	<jsp:include page="./../anime-main/footer.jsp"/>
+	<jsp:include page="./../anime-main/header.jsp" />
+
+	<!-- Normal Breadcrumb Begin -->
+	<section class="normal-breadcrumb set-bg"
+		data-setbg="<%=contextPath%>img/normal-breadcrumb.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="text-center">
+					<div class="normal__breadcrumb__text">
+						<h2>단돈 만원으로 제공되는</h2>
+						<h2>한달간의 무제한 스트리밍 서비스</h2>
+						<p>Welcome to the CINEBRO</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Normal Breadcrumb End -->
+
+	<section class="anime-details spad">
+		<div class="container">
+			<div class="text-center">
+				<div class="anime__details__btn" align="center">
+					<a href="#" class="follow-btn"> <i
+						style="font-size: 48px; color: black;"> <b>cinebro 구독하러가기</b>
+					</i>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<jsp:include page="./../anime-main/footer.jsp" />
 </body>
 </html>
--- Cinebro/src/cinebro/members/controller/MemberLogOutController.java
+++ Cinebro/src/cinebro/members/controller/MemberLogOutController.java
@@ -18,21 +18,21 @@ public class MemberLogOutController extends SuperClass {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doGet(request, response);
 		
-		// 만약 세션 영역에 장바구니 정보가 들어 있으면
-		// 이 정보를 장바구니 임시 테이블에 저장합니다.
-		FilmListDao mycart = (FilmListDao)super.session.getAttribute("mycart");
-		
-		if (mycart != null) {
-			Map<Integer, Integer> maplist = mycart.GetFilmList() ;
-			
-			MemberDao mem = (MemberDao)super.session.getAttribute("loginfo");
-			
-			MemberDao dao = new MemberDao();
-			dao.InsertCartData(mem, maplist) ;
-		}
-		
-		// 나의 로그인 정보 및 관련 모든 것들을 비웁니다.
-		super.session.invalidate(); 
+//		// 만약 세션 영역에 장바구니 정보가 들어 있으면
+//		// 이 정보를 장바구니 임시 테이블에 저장합니다.
+//		FilmListDao mycart = (FilmListDao)super.session.getAttribute("mycart");
+//		
+//		if (mycart != null) {
+//			Map<Integer, Integer> maplist = mycart.GetFilmList() ;
+//			
+//			MemberDao mem = (MemberDao)super.session.getAttribute("loginfo");
+//			
+//			MemberDao dao = new MemberDao();
+//			dao.InsertCartData(mem, maplist) ;
+//		}
+//		
+//		// 나의 로그인 정보 및 관련 모든 것들을 비웁니다.
+//		super.session.invalidate(); 
 		
 		String gotopage = "/member/meLoginForm.jsp" ;
 		super.GotoPage(gotopage);
