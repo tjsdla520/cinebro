4d028ca27c19c54cbb0d2f0d5da839a6f7c3f19d
kang-10067790 <kangbm1108@naver.com>
2021-05-13 16:48:21 +0900

    mywish

--- Cinebro/src/cinebro/profile/controller/MyWishController.java
+++ Cinebro/src/cinebro/profile/controller/MyWishController.java
@@ -7,19 +7,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import cinebro.common.controller.SuperClass;
+import cinebro.members.model.Member;
+import cinebro.profile.model.ProfileDao;
+import cinebro.reviews.model.ReviewDao;
 
 public class MyWishController extends SuperClass {
 	
-	@Override
+    @Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doGet(request, response);
 		
-		dao.selectMywish(email);
+		 
+		
+		String gotopage = "/profile/myWish.jsp" ;
+		super.GotoPage(gotopage);
+		
 	}
     
 	
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doPost(request, response);
+
 	}	
 }
