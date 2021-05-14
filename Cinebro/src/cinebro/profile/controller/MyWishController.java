891a0ef5c255e6a78406dfaf81d7c1bf635413ce
kang-10067790 <kangbm1108@naver.com>
2021-05-14 09:41:46 +0900

    MyWishController

--- Cinebro/src/cinebro/profile/controller/MyWishController.java
+++ Cinebro/src/cinebro/profile/controller/MyWishController.java
@@ -1,24 +1,35 @@
 package cinebro.profile.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import cinebro.common.controller.SuperClass;
-import cinebro.members.model.Member;
+import cinebro.profile.model.Profile;
 import cinebro.profile.model.ProfileDao;
-import cinebro.reviews.model.ReviewDao;
+import cinebro.reviews.model.Review;
+
 
 public class MyWishController extends SuperClass {
+	//wishfilms
 	
     @Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doGet(request, response);
+		Profile bean = new Profile();
+		ProfileDao dao = new ProfileDao();
 		
-		 
+	
+	List<Profile> lists = new ArrayList<Profile>() ;
 		
+		request.setAttribute("bean", bean);
+		
+
 		String gotopage = "/profile/myWish.jsp" ;
 		super.GotoPage(gotopage);
 		
@@ -28,6 +39,8 @@ public class MyWishController extends SuperClass {
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doPost(request, response);
+		Profile bean = new Profile();
+		ProfileDao dao = new ProfileDao();
 
 	}	
 }
